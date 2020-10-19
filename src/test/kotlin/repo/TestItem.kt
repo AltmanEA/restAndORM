fb6d25ddaa9a80a9bddb0816e24640c90f506e0e
Евгений Альтман <altmanea@gmail.com>
2020-10-19 11:38:50 +0600

    Initial commit

---
+++ src/test/kotlin/repo/TestItem.kt
@@ -0,0 +1,38 @@
+package repo
+
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.statements.UpdateBuilder
+
+class TestItem(
+    val name: String,
+    override var id: Int = -1
+) : Item
+
+class TestItemTable : RepoDSLTable<TestItem>() {
+    val name = varchar("name", 50)
+    override fun fill(builder: UpdateBuilder<Int>, item: TestItem) {
+        builder[name] = item.name
+    }
+    override fun readResult(result: ResultRow) =
+        TestItem(
+            result[name],
+            result[id].value
+        )
+}
+
+val testItemTable = TestItemTable()
+
+class TestItemClass(id: EntityID<Int>) : ItemClass<TestItem>(id) {
+    companion object : IntEntityClass<TestItemClass>(testItemTable) {}
+
+    var name by testItemTable.name
+
+    override val dto: TestItem
+        get() = TestItem(name, id.value)
+
+    override fun fill(item: TestItem) {
+        name = item.name
+    }
+}
